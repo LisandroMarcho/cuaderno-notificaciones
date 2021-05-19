732c3dd4885f7a2917ae8a803046de1e36e98220
LisandroMarcho <djlm2140@gmail.com>
2021-05-19 16:41:53 -0300

    Test firebase

--- components/BotonInicio.jsx
+++ components/BotonInicio.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import {TouchableOpacity, Text} from 'react-native'
+import {TouchableOpacity, Text, StyleSheet} from 'react-native'
+import firebase from '../firebase';
+
+const dbRef = firebase.database().ref().child('test');
+
+function agregarObject () {
+  dbRef.push({
+    test: 'Hola mundo'
+  })
+}
 
 export default function BotonInicio ({children}) {
+    const Styles = StyleSheet.create({
+      boton: {
+        width:'100%',
+        height: 80,
+        padding: 30,
+        textAlign: 'center',
+        backgroundColor: '#30A4FF',
+        borderRadius: 15,
+        marginBottom: 25,
+        borderColor: 'black',
+        borderWidth: 1
+      },
+      text: {
+        textAlign: 'center', fontSize: 16
+      }
+    })
     return(
-    <TouchableOpacity style={{
-            width:'100%',
-            height: 80,
-            padding: 30,
-            textAlign: 'center',
-            backgroundColor: '#30A4FF',
-            borderRadius: 15,
-            marginBottom: 25,
-            borderColor: 'black',
-            borderWidth: 1
-        }}>
-        <Text style={{textAlign: 'center', fontSize: 16}}>{children}</Text>
+    <TouchableOpacity style={Styles.boton} onPress={agregarObject}>
+        <Text style={Styles.text}>{children}</Text>
     </TouchableOpacity>           
     )
 }
