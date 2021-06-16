3473717477423f3c905f6fd009f51497344ee95f
LisandroMarcho <djlm2140@gmail.com>
2021-06-16 10:52:08 -0300

    index test

---
+++ www/js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACY7-nrXyrAxbYRP2-c5I34UNJOV3S4e8",
+  authDomain: "cuaderno-comunicaciones.firebaseapp.com",
+  databaseURL: "https://cuaderno-comunicaciones-default-rtdb.firebaseio.com",
+  projectId: "cuaderno-comunicaciones",
+  storageBucket: "cuaderno-comunicaciones.appspot.com",
+  messagingSenderId: "153672834962",
+  appId: "1:153672834962:web:7c04e421f2c9557a8cab4b",
+};
+
+firebase.initializeApp(firebaseConfig);
