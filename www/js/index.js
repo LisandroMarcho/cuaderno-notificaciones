9bfc8060e4c746b33e44b34392d9c5dd0ae4009f
Zenitsuagatsuma40 <sebas3277xd@gmail.com>
2021-06-23 09:55:14 -0300

    cambios js

--- www/js/index.js
+++ www/js/index.js
@@ -6,5 +6,5 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    // document.getElementById('deviceready').classList.add('ready');
+    // document.getElementById('deviceready').classList.add('ready'); 
 }
