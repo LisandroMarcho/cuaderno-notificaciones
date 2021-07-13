a09f9079f804ba2342af6c8c279a0a73cbd62609
LisandroMarcho <djlm2140@gmail.com>
2021-07-13 07:52:50 -0300

    sin terminar

--- www/js/Alumno.js
+++ www/js/Alumno.js
@@ -1,8 +1,10 @@
+const alumnosRef = firebase.database().ref("usuarios/alumnos/");
+
 class Alumno {
   /**
-   * Inicializa una nueva instancia de Alumno, y permite manipular su información. Además, hace 
+   * Inicializa una nueva instancia de Alumno, y permite manipular su información. Además, hace
    * una copia del usuario de Firebase.
-   *  
+   *
    * @example
    * const user = new Alumno();
    */
@@ -15,19 +17,23 @@ class Alumno {
    * TODO: Vincular dni con el usuario
    * @param {object} credentials            Objeto con las credenciales
    * @param {String} credentials.email      Correo electronico del usuario
+   * @param {Number} credentials.dni        DNI del usuario
    * @param {String} credentials.password   Contraseña del usuario
    * @example
    * const credenciales = {
    *  email: 'mail@example.com',
    *  password: 'example_password'
    * };
-   * 
+   *
    * const result = await Alumno.registrar(credenciales);
    * @returns {Object} User or error
    */
-  static async registrar({ email, password }) {
+  static async registrar({ email, dni, password }) {
     try {
-      const userCredential = await fireAuth().createUserWithEmailAndPassword(email, password);
+      const userCredential = await fireAuth().createUserWithEmailAndPassword(
+        email,
+        password
+      );
       return userCredential.user;
     } catch (error) {
       const errorCode = await error.code;
@@ -38,39 +44,39 @@ class Alumno {
   /**
    * Autentica un usuario con un email y contraseña
    * @param {Object} credentials            Objeto con las credenciales
-   * @param {String} credentials.email      Correo electronico del usuario
+   * @param {Number} credentials.dni        DNI del usuario
    * @param {String} credentials.password   Contraseña del usuario
    * @example
    * const credenciales = {
    *  email: 'mail@example.com',
    *  password: 'example_password'
    * };
-   * 
+   *
    * const result = await Alumno.iniciarSesion(credenciales);
    * @returns {Object} User or error
    */
-  static async iniciarSesion({ email, password }) {
+  static async iniciarSesion({ dni, password }) {
     try {
-      const userCredential = await fireAuth().signInWithEmailAndPassword(email, password);
+      const alumnoRef = await alumnosRef.child(dni).get();
+      const { email } = alumnoRef.val();
+      const userCredential = await fireAuth().signInWithEmailAndPassword(
+        email,
+        password
+      );
       return userCredential.user;
     } catch (error) {
       const errorCode = error.code;
-      return { error:errorCode }
+      return { error: errorCode };
     }
   }
 
   /**
-   * Cierra la sesión del alumno. El cambio de estado de usuario se puede ver 
+   * Cierra la sesión del alumno. El cambio de estado de usuario se puede ver
    * reflejado en el listener ```fireAuth().onAuthStateChanged((user) => {...})```.
    * @example
    * Alumno.cerrarSesion();
    */
-  static async cerrarSesion() {
-    await fireAuth().signOut();
+  static cerrarSesion() {
+    fireAuth().signOut();
   }
 }
-
-// fireAuth().onAuthStateChanged((user)=>{
-//   if(user !== null) console.log(user.uid)
-//   else console.log(null)
-// })
--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,28 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+// document.addEventListener("deviceready", onDeviceReady, false);
 
-/**
- * Funcion por defecto de Cordova para cuando se pueden usar las API
- */
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+// /**
+// Funcion por defecto de Cordova para cuando se pueden usar las API
+// /
+// function onDeviceReady() {
 
-    // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    // document.getElementById('deviceready').classList.add('ready'); 
+// }
+
+window.onload = () => {
+  firebase.auth().onAuthStateChanged(isLoggedIn);
+};
+
+function isLoggedIn(user) {
+  const pathname = window.location.pathname;
+  const filename = pathname.slice(pathname.lastIndexOf("/") + 1);
+
+  if (user) {
+    console.log(user.uid);
+  }
+
+  // Si no hay un usuario
+  if (user == null) {
+    console.log(null);
+  }
 }
