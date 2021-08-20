ac6b84b6118c3a3bfffa87dddc1440cc2c88f1fd
LisandroMarcho <djlm2140@gmail.com>
2021-08-20 13:55:06 -0300

    Fix a medias

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
-const alumnosRef = firebase.database().ref("usuarios/alumnos/");
-const avisosRef  = firebase.database().ref("avisos/");
+const cursosRef = firebase.database().ref("cursos/");
+// const avisosRef = firebase.database().ref("avisos/");
 //const chatRef    = firebase.database().ref("chat/");
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
@@ -13,153 +13,83 @@ const avisosRef  = firebase.database().ref("avisos/");
 
 // }
 
-class Alumno {
-  /**
-   * Inicializa una nueva instancia de Alumno, y permite manipular su información. Además, hace
-   * una copia del usuario de Firebase.
-   *
-   * @example
-   * const user = new Alumno();
-   */
-  constructor() {
-    this.firebaseUser = fireAuth().currentUser;
-  }
-
-  /**
-   * Registra un usuario con email y contraseña.
-   * TODO: Vincular dni con el usuario
-   * @param {Object} credentials            Objeto con las credenciales
-   * @param {String} credentials.email      Correo electronico del usuario
-   * @param {Number} credentials.dni        DNI del usuario
-   * @param {String} credentials.password   Contraseña del usuario
-   * @example
-   * const credenciales = {
-   *  email: 'mail@example.com',
-   *  password: 'example_password'
-   * };
-   *
-   * const result = await Alumno.registrar(credenciales);
-   * @returns {Object} User or error
-   */
-  static registrar({ email, dni, password }) {
-    fireAuth().createUserWithEmailAndPassword(email, password)
-      .then(async (user) => {
-        await alumnosRef.child(dni).update({ email });
-        localStorage.setItem('infoAlumno', JSON.stringify({  }))
-        return user;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  /**
-   * Autentica un usuario con un email y contraseña
-   * @param {Object} credentials            Objeto con las credenciales
-   * @param {Number} credentials.dni        DNI del usuario
-   * @param {String} credentials.password   Contraseña del usuario
-   * @example
-   * const credenciales = {
-   *  email: 'mail@example.com',
-   *  password: 'example_password'
-   * };
-   *
-   * const result = await Alumno.iniciarSesion(credenciales);
-   * @returns {Object} User or error
-   */
-  static async iniciarSesion({ dni, password }) {
-    const alumnoRef = await alumnosRef.child(dni).get();
-    const { email } = alumnoRef.val();
-    const userCredential = await fireAuth().signInWithEmailAndPassword(
-      email,
-      password
-    );
-    return userCredential.user;
-  }
-
-  /**
-   * Cierra la sesión del alumno.
-   * @example
-   * Alumno.cerrarSesion();
-   */
-  static cerrarSesion() {
-    fireAuth().signOut();
-  }
-}
-
-class Aviso {
-  /**
-   * @param {Object} infoCurso            Objeto contenedor de la info del curso
-   * @param {Number} infoCurso.curso      Año que del curso
-   * @param {Number} infoCurso.division   Division del curso
-   * @param {Object} bodyAviso            Objeto contenedor del cuerpo del aviso
-   * @param {String} bodyAviso.titulo
-   * @param {String} bodyAviso.texto
-   * @param {String} bodyAviso.rolCreador
-   * @param {String} body
-   */
-  constructor (infoCurso, bodyAviso) {
-    this.infoCurso = infoCurso;
-    this.body = bodyAviso;
-  }
-
-  static async crear() {
-    await avisosRef.child(this.infoCurso.curso)
-      .child(this.infoCurso.division)
-      .push(this.body);
-  }
-
-  static async traerTodos({ curso, division }) {
-    const avisos = await avisosRef.child(curso).child(division).get();
-    const avisosVal = avisos.val();
-    return avisosVal;
-  }
-}
-
-class Chat {
-  constructor(idChat) {
-    this.chat = document.getElementById(idChat);
-    this.mensajes = document.getElementById('mensajes');
-    this.controles = document.getElementById('controles');
-  }
-
-  enviarMensaje() {
-    let userEmail = fireAuth().currentUser.email;
-    userEmail = userEmail.replaceAll('.', '-');
-    chatRef.child(userEmail).child('mensajes').push({
-      body: 'jeje'
+/**
+ * Registra un usuario con email y contraseña.
+ * TODO: Vincular dni con el usuario
+ * @param {Object} credentials            Objeto con las credenciales
+ * @param {String} credentials.email      Correo electronico del usuario
+ * @param {Number} credentials.dni        DNI del usuario
+ * @param {String} credentials.password   Contraseña del usuario
+ * @example
+ * const credenciales = {
+ *  email: 'mail@example.com',
+ *  password: 'example_password'
+ * };
+ *
+ * const result = await Alumno.registrar(credenciales);
+ * @returns {Object} User or error
+ */
+function registrarAlumno({ email, dni, password }) {
+  firebase.auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(async (user) => {
+      await alumnosRef.child(dni).update({ email });
+      localStorage.setItem("infoAlumno", JSON.stringify({}));
+      return user;
     })
-  }
+    .catch((e) => {
+      console.log(e);
+    });
+}
 
-  mostrarMensaje(msg, esEnviado = true) {
-    let nuevoMensaje = document.createElement('div');
-    nuevoMensaje.classList.add('mensaje');
-    nuevoMensaje.classList.add(esEnviado ? 'msg-r' : 'msg-l');
-  
-    let textoMensaje = document.createElement('p');
-    if(msg === '') return false;
-    textoMensaje.innerText = msg;
-    
-    nuevoMensaje.appendChild(textoMensaje);
-    this.mensajes.appendChild(nuevoMensaje);
-  }
+/**
+ * Autentica un usuario con un email y contraseña
+ * @param {Object} credentials            Objeto con las credenciales
+ * @param {Number} credentials.dni        DNI del usuario
+ * @param {String} credentials.password   Contraseña del usuario
+ * @example
+ * const credenciales = {
+ *  email: 'mail@example.com',
+ *  password: 'example_password'
+ * };
+ *
+ * const result = await Alumno.iniciarSesion(credenciales);
+ * @returns {Object} User or error
+ */
+async function iniciarSesion({ dni, password }) {
+  const alumnoRef = await alumnosRef.child(dni).get();
+  const { email } = alumnoRef.val();
+  const userCredential = await firebase.auth().signInWithEmailAndPassword(
+    email,
+    password
+  );
+  return userCredential.user;
+}
+
+/**
+ * Cierra la sesión del alumno.
+ * @example
+ * Alumno.cerrarSesion();
+ */
+function cerrarSesion() {
+  firebase.auth().signOut();
 }
 
 /**
  * Verifica si el cliente tiene una sesión iniciada
- * @param {Object} user 
+ * @param {Object} user
  */
 function isLoggedIn(user) {
   const pathname = window.location.pathname;
   const filename = pathname.slice(pathname.lastIndexOf("/") + 1);
 
-  if(user == null) {
-    if(filename !== 'login.html' && filename !== 'registro.html') {
-      window.location = 'login.html';
+  if (user == null) {
+    if (filename !== "login.html" && filename !== "registro.html") {
+      window.location = "login.html";
     }
   } else {
-    if(filename === 'login.html' || filename === 'registro.html') {
-      window.location = 'index.html';
+    if (filename === "login.html" || filename === "registro.html") {
+      window.location = "index.html";
     }
   }
 }
@@ -167,36 +97,30 @@ function isLoggedIn(user) {
 /**
  * Inicializa la applicación
  */
-function initializeApp (){
+function initializeApp() {
   let sidebar = document.querySelector(".sidebar");
   let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
 
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();//calling the function(optional)
-  });
+  // firebase.auth().onAuthStateChanged(isLoggedIn);
 
-  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+  closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
     menuBtnChange(); //calling the function(optional)
   });
 
-  firebase.auth().onAuthStateChanged(isLoggedIn);
-  
-  const initialyDisabledElements = document.querySelectorAll('.init-disabled');
-  initialyDisabledElements.forEach((element) => {
-    element.classList.remove('init-disabled');
+  const initiallyDisabledElements = document.querySelectorAll(".init-disabled");
+
+  initiallyDisabledElements.forEach((element) => {
+    element.classList.remove("init-disabled");
     element.disabled = false;
-  })
-};
+  });
+}
 
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
-  if(sidebar.classList.contains("open")){
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-  }else {
-    closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+  } else {
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
   }
 }
-
