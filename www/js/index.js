e782883cdf6a95f0805d9c031b8407cd2572667b
Zenitsuagatsuma40 <sebas3277xd@gmail.com>
2021-08-23 08:43:34 -0300

    ajuste del css login

--- www/js/index.js
+++ www/js/index.js
@@ -170,16 +170,11 @@ function isLoggedIn(user) {
 function initializeApp (){
   let sidebar = document.querySelector(".sidebar");
   let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
-
+ /* let searchBtn = document.querySelector(".bx-search");
+*/
   closeBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("open");
     menuBtnChange();//calling the function(optional)
-  });
-
-  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
   });
 
   firebase.auth().onAuthStateChanged(isLoggedIn);
